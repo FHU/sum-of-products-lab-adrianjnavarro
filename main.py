2ea2ee35870ffd8da096b48eb4a5ef15f0c1b29a
Adrian <adrian.navarro@students.fhu.edu>
2024-01-30 10:20:31 -0500

    Update main.py
    Removed text

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ def sum_of_products(list1, list2):
     if len(list1) != len(list2):
         return "Error"
     
-    return sum(a * b for a, b in zip(list1,list2))
+    return sum(a * b for a, b in zip(list1, list2))
+    return result
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    input_string1 = input("Enter the first series of numbers: ")
-    input_string2 = input("Enter the second series of numbers: ")
+    input_string1 = input()
+    input_string2 = input()
 
     list1 = [int(char) for char in input_string1 if char.isdigit()]
     list2 = [int(char) for char in input_string2 if char.isdigit()]
 
     result = sum_of_products(list1, list2)
-    print("Output =", result)
+    print(result)
 
