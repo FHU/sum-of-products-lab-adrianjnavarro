c82d4e148061e96d1c991ae61fe314632095e86e
Adrian <adrian.navarro@students.fhu.edu>
2024-01-30 17:52:15 -0500

    Update main.py
    Changed digit to split (lines 15 - 16)

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
     input_string1 = input()
     input_string2 = input()
 
-    list1 = [int(char) for char in input_string1 if char.isdigit()]
-    list2 = [int(char) for char in input_string2 if char.isdigit()]
+    list1 = [int(char) for char in input_string1 if char.split()]
+    list2 = [int(char) for char in input_string2 if char.split()]
 
     result = sum_of_products(list1, list2)
     print(result)
